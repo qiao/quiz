442edf62e3674652bfb9d7b62522342b8215b632
Joe Xu <xueqiaoxu@gmail.com>
2026-09-16 22:07:36 -0700

    promo: type /quiz this repo as the command

--- promo/src/scenes/CommandScene.tsx
+++ promo/src/scenes/CommandScene.tsx
@@ -3,7 +3,7 @@ import { mouseClick } from "@remotion/sfx";
 import { AbsoluteFill, Interactive, Sequence, useCurrentFrame } from "remotion";
 import { COLOR, MONO } from "../theme";
 
-const COMMAND = "/quiz this React app";
+const COMMAND = "/quiz this repo";
 
 /** Frame where the first character of the command appears. */
 const TYPE_START = 10;
